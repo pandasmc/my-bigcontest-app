fbc0b247f9669da27a96cad5d78414bfbade6cc7
pandasmc <pandasmc@naver.com>
2025-10-24 15:05:43 +0900

    Update app.py

--- app.py
+++ app.py
@@ -398,8 +398,8 @@ def show_report(store_data, data):
         x = range(len(months))
 
         # --- 차트 크기/너비 설정 ---
-        CHART_FIGSIZE = (6, 3.5) 
-        CHART_WIDTH = 550        
+        CHART_FIGSIZE = (8, 4) 
+        CHART_WIDTH = 800  
         # ---------------------------
 
         st.subheader("고객 및 상권 동향")
